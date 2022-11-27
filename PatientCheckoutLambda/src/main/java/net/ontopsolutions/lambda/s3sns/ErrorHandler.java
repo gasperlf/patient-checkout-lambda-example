0ab57ef531c6f19ab4bdf4438aa80dc5c3526474
gasperlf <gasper_lf@hotmail.com>
2022-11-27 17:32:38 +0100

    adding dead letter queue

---
+++ PatientCheckoutLambda/src/main/java/net/ontopsolutions/lambda/s3sns/ErrorHandler.java
@@ -0,0 +1,10 @@
+package net.ontopsolutions.lambda.s3sns;
+
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+
+public class ErrorHandler {
+
+    public void handler(SNSEvent snsEvent) {
+        // log all data from records
+    }
+}
